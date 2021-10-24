2ec853b924b5385c58a8e388d8291a3b6a931eb0
Joni Honkanen <joni.honkanen@tuni.fi>
2021-10-24 13:24:52 +0300

    Add comments on Program.java function calls.

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,11 @@
 class Program {
     public static void main(String[] args) {
+        //Retrieve the user name
         Class1 name = new Class1();
         String text = name.tellName();
         System.out.println(text);
         
+        //Retrieve user age
         Class2 number = new Class2();
         int age = number.tellAge();
         System.out.println(age);
